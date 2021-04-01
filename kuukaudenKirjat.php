fb5f598a607954c42d167790ec8c61f9a88052c4
Aleksi Hannula <n9haal00@students.oamk.fi>
2021-04-01 11:30:03 +0300

    Kuukauden kirjat

---
+++ kuukaudenKirjat.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'inc/functions.php';
+require_once 'inc/headers.php';
+
+try{
+    $db=opendb();
+    jsonFactory($db,"select DISTINCT(kirjaNimi), sivuNro, hinta, kuvaus, kuva, YEAR(julkaistu) as 'vuosi', etunimi, sukunimi, julkaisija.julkaisija, kategoria.kategoria
+    from kirja
+        inner join kirjakategoria
+            on kirja.kirjaNro = kirjakategoria.kirjaNro
+        inner join kategoria
+            on kirjakategoria.kategoriaNro = kategoria.kategoriaNro
+        inner join julkaisija
+            on kirja.julkaisijaNro = julkaisija.julkaisijaNro
+        inner join kirjailijakirja
+            on kirja.kirjaNro = kirjailijakirja.kirjaNro
+        inner join kirjailija
+            on kirjailijakirja.kirjailijaNro = kirjailija.kirjailijaNro
+    GROUP BY kirjaNimi
+    HAVING vuosi =" . date("Y") . "");
+} catch (PDOException $pdoex) {
+    returnError($pdoex); 
+}
