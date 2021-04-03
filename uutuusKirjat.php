65db5716e3b3e26bc8f51ec89b01a29871c4d05b
Aleksi Hannula <n9haal00@students.oamk.fi>
2021-04-03 16:23:56 +0300

    Uutuuskirjat

--- uutuusKirjat.php
+++ uutuusKirjat.php
@@ -1,4 +1,10 @@
 <?php
+
+require_once 'inc/functions.php';
+require_once 'inc/headers.php';
+
+$input = json_decode(file_get_contents('php://input'));
+
 try{
     $db=opendb();
     jsonFactory($db,"select DISTINCT(kirjaNimi), sivuNro, hinta, kuvaus, kuva, YEAR(julkaistu) as 'vuosi', etunimi, sukunimi, julkaisija.julkaisija, kategoria.kategoria
