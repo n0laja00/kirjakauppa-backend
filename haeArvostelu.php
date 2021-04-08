8e0d384904f5770e73d026912a56aa31491707a7
Riku Pitkanen <n0piri00@students.oamk.fi>
2021-04-08 16:41:52 +0300

    hae arvostelu php

---
+++ haeArvostelu.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once 'inc/functions.php';
+require_once 'inc/headers.php';
+
+
+// lue parametrit URLIsta
+$url = parse_url(filter_input(INPUT_SERVER, 'PATH_INFO', FILTER_SANITIZE_NUMBER_INT), PHP_URL_PATH);
+// Parametrit erotellaan /ssilla
+$parametrit = explode('/', $url);
+// kategoria on ensimmäinen parametri,joka seuraa osoitteen jälkeen ja eroteltu: /
+$arvosteluId = $url[0];
+
+try {
+    $db = opendb();
+    jsonFactory($db, "select * from arvostelu where arvosteluNro = $arvosteluId");
+} catch (PDOException $pdoex) {
+    returnError($pdoex);
+};
