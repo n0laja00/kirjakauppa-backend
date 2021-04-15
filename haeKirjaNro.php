6e286f3a848811274ad9d6e3d9860c6bd9a223d3
Riku Pitkanen <n0piri00@students.oamk.fi>
2021-04-15 14:42:59 +0300

    korjattu parametrin parse

--- haeKirjaNro.php
+++ haeKirjaNro.php
@@ -5,14 +5,15 @@ require_once 'inc/headers.php';
 // lue parametrit URLIsta
 $url = parse_url(filter_input(INPUT_SERVER, 'PATH_INFO', FILTER_SANITIZE_NUMBER_INT), PHP_URL_PATH);
 // Parametrit erotellaan /ssilla
-$parametrit = explode('/',$url);
+$parametrit = explode('/', $url);
 // kategoria on ensimmäinen parametri,joka seuraa osoitteen jälkeen ja eroteltu: /
-$kirjaNro_id=$url[0];
+
+$kirjaNro_id = $parametrit[0];
 
 
-try{
-    $db=opendb();
-    jsonFactory($db,"select DISTINCT(kirjaNimi), kirja.kirjaNro, sivuNro, hinta, kuvaus, kuva, YEAR(julkaistu) AS julkaistu, etunimi, sukunimi, julkaisija.julkaisija, kategoria.kategoria
+try {
+    $db = opendb();
+    jsonFactory($db, "select DISTINCT(kirjaNimi), kirja.kirjaNro, sivuNro, hinta, kuvaus, kuva, YEAR(julkaistu) AS julkaistu, etunimi, sukunimi, julkaisija.julkaisija, kategoria.kategoria
     from kirja
         inner join kirjakategoria
             on kirja.kirjaNro = kirjakategoria.kirjaNro
@@ -27,7 +28,7 @@ try{
     where kirja.kirjaNro=$kirjaNro_id
     group by kirjaNimi");
 } catch (PDOException $pdoex) {
-    returnError($pdoex); 
+    returnError($pdoex);
 };
 
 /*
